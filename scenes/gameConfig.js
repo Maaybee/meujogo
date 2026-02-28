394485688a036615334ff29aca889923b19c8b7e
Stephany Marques <ster.mark1@gmail.com>
2026-02-28 00:01:09 -0300

    Delete scenes/gameConfig.js

--- scenes/gameConfig.js
+++
@@ -1,8 +0,0 @@
-// Cria uma chave para cada uma das cenas.
-export const SCENES = { 
-    START: 'startScene', // Nome da chave + nome da classe construtora dela.
-    GAME: 'gameScene',
-    INSTRUCTIONS: 'instructionsScene',
-    INTRO: 'introScene',
-    END: 'endScene'
-};
